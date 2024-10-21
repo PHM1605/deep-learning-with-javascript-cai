4ef50e0e2bd01dfa85852e2efdbc1083e53a7965
PHM1605 <PHM1605@gmail.com>
2024-10-22 00:38:32 +0700

    updates

--- 2-1-download-time-prediction/index.js
+++ 2-1-download-time-prediction/index.js
@@ -12,6 +12,15 @@ trainYs = tf.tensor2d(trainData.timeSec, [20, 1]);
 testXs = tf.tensor2d(testData.sizeMB, [20, 1]);
 testYs = tf.tensor2d(testData.timeSec, [20, 1]);
 
+const trainTensors = {
+  sizeMB: tf.tensor2d(trainData.sizeMB, [20, 1]),
+  timeSec: tf.tensor2d(trainData.timeSec, [20, 1])
+};
+const testTensors = {
+  sizeMB: tf.tensor2d(testData.sizeMB, [20, 1]),
+  timeSec: tf.tensor2d(testData.timeSec, [20, 1])
+}
+
 const dataTraceTrain = {
   x: trainData.sizeMB, y: trainData.timeSec,
   name: 'trainData', mode: 'markers', type: 'scatter', marker: {symbol: 'circle', size: 8}
@@ -20,12 +29,13 @@ const dataTraceTest = {
   x: testData.sizeMB, y: testData.timeSec,
   name: 'testData', mode: 'markers', type: 'scatter', marker: {symbol: 'triangle-up', size: 10}
 };
-// // plot new 
-// Plotly.newPlot(
-//   'dataSpace', 
-//   [dataTraceTrain, dataTraceTest], 
-//   {width: 700, title: 'File download duration', xaxis: {title: 'size (MB)'}, yaxis: {title: "time (sec)"}}
-// )
+
+function updateScatterWithLines(dataTrace, k, b, N, traceIndex) {
+  dataTrace.x = [0, 10];
+  dataTrace.y = [b, b+k*10];
+  var update = {x: [dataTrace.x], y: [dataTrace.y], name: "model after " + N + " epochs"}
+  Plotly.restyle('dataSpace', update, traceIndex)
+}
 
 const dataTrace10Epochs = {
   x: [0, 2], y: [0, 0.01],
@@ -55,4 +65,32 @@ Plotly.newPlot(
 const model = tf.sequential();
 model.add(tf.layers.dense({units: 1, inputShape: [1]}))
 model.compile({optimizer: tf.train.sgd(0.0005), loss:"meanAbsoluteError"});
+let k = 0;
+let b = 0;
+model.setWeights([ tf.tensor2d([k], [1,1]), tf.tensor1d([b]) ]);
+(async () => {
+  await model.fit(trainTensors.sizeMB, trainTensors.timeSec, {
+    epochs: 200, 
+    callbacks: {
+      onEpochEnd: async (epoch, logs) => {
+        k = model.getWeights()[0].dataSync()[0]
+        b = model.getWeights()[1].dataSync()[0]
+        if (epoch === 9) {
+          updateScatterWithLines(dataTrace10Epochs, k, b, 10, 2);
+          console.log("wrote model 10")
+        }
+        if (epoch===19) {
+          updateScatterWithLines(dataTrace20Epochs, k, b, 20, 3);
+          console.log("wrote model 20")
+        }
+        if (epoch === 199) {
+          updateScatterWithLines(dataTrace200Epochs, k, b, 200, 4);
+          console.log("wrote model 200")
+        }
+        await tf.nextFrame();
+      }
+    }
+  })
+})();
 
+model.evaluate(testTensors.sizeMB, testTensors.timeSec).print()
