bc5398027a950863d8649f3edc18a500fd0bf704
RTDevOps <RTDevOps@dmspro.vn>
2024-10-24 17:36:41 +0700

    updates

--- 2-1-download-time-prediction/index.js
+++ 2-1-download-time-prediction/index.js
@@ -37,10 +37,6 @@ function updateScatterWithLines(dataTrace, k, b, N, traceIndex) {
   Plotly.restyle('dataSpace', update, traceIndex)
 }
 
-function setModelWeights(k, b) {
-  model.setWeights([tf.tensor2d([k], [1,1]), tf.tensor1d([b])])
-}
-
 const dataTrace10Epochs = {
   x: [0, 2], y: [0, 0.01],
   name: 'model after 10 epochs',
@@ -53,16 +49,22 @@ const dataTrace20Epochs = {
   mode: 'lines',
   line: {color: 'green', width:2, dash:"dash"}
 }
+const dataTrace100Epochs = {
+  x: [0, 2], y: [0, 0.01],
+  name: 'model after 100 epochs',
+  mode: 'lines',
+  line: {color: 'red', width:3, dash:'long-dash'}
+}
 const dataTrace200Epochs = {
   x: [0, 2], y: [0, 0.01],
   name: 'model after 200 epochs',
   mode: 'lines',
-  line: {color: 'red', width:3, dash:"solid"}
+  line: {color: 'black', width:4, dash:"solid"}
 }
 
 Plotly.newPlot(
   'dataSpace',
-  [dataTraceTrain, dataTraceTest, dataTrace10Epochs, dataTrace20Epochs, dataTrace200Epochs],
+  [dataTraceTrain, dataTraceTest, dataTrace10Epochs, dataTrace20Epochs, dataTrace100Epochs, dataTrace200Epochs],
   {width: 700, title:"Model fit result", xaxis:{title:"size (MB)"}, yaxis:{title:"time (sec)"}}
 )
 
@@ -87,6 +89,26 @@ function plotLoss(epoch, loss) {
   })
 }
 
+// Plot params
+const kTrace = {x:[], y:[], name:"kernel"}
+const bTrace = {x:[], y:[], name:"bias"}
+function plotParams(epoch, k, b) {
+  kTrace.x.push(epoch);
+  kTrace.y.push(k);
+  bTrace.x.push(epoch);
+  bTrace.y.push(b);
+  Plotly.newPlot('paramsPlot', [kTrace, bTrace], {width:400, xaxis:{title:"epoch #"}})
+}
+
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 1, inputShape: [1]}))
+model.summary()
+model.compile({optimizer: tf.train.sgd(0.0005), loss:"meanAbsoluteError"});
+
+function setModelWeights(k, b) {
+  model.setWeights([tf.tensor2d([k], [1,1]), tf.tensor1d([b])])
+}
+
 // Prepare loss surface 2d
 const landscape = {x:[], y:[], z:[], type:'contour'}
 function genLossLandscape() {
@@ -107,24 +129,39 @@ function genLossLandscape() {
 
 // Plot loss surface 2d with trajectory
 const trajectory = {x:[], y:[], type:"scatter"}
+const trace0 = {x:[], y:[], name:"minimum by eye", mode:"markers", type:"scatter", marker:{symbol:"circle", size:10, color:"white"}}
+const trace20 = {x:[], y:[], name:"minimum by eye", mode:"markers", type:"scatter", marker:{symbol:"circle", size:10, color:"white"}}
+const trace100 = {x:[], y:[], name:"minimum by eye", mode:"markers", type:"scatter", marker:{symbol:"circle", size:10, color:"white"}}
+const trace200 = {x:[], y:[], name:"minimum by eye", mode:"markers", type:"scatter", marker:{symbol:"circle", size:10, color:"white"}}
+
 function recordTrajectory(k, b, loss) {
   trajectory.x.push(k);
   trajectory.y.push(b);
   contourAnnotationFont = {family:"sans serif", size:18, color:"#ffffff"};
-  Plotly.newPlot('lossSurface2dWithTraj', [landscape, trajectory], {
+  Plotly.newPlot('lossSurface2dWithTraj', [landscape, trajectory, trace0, trace20, trace100, trace200], {
     height:500, width:500, title:"Loss surface", xaxis:{title:"kernel"}, yaxis:{title:"bias"}, showlegend:false,
-    annotations:[]
+    annotations:[
+      {x:0, y:0, xref:"x", yref:"y", text:"Start", showarrow:true, arrowhead:0, arrowcolor:"#ffffff", font:contourAnnotationFont, ax:0, ay:-40},
+      {x:0.028, y:0.01, xref:"x", yref:"y", text:"Epoch 20", showarrow:true, arrowhead:0, arrowcolor:"#ffffff", font:contourAnnotationFont, ax:0, ay:40},
+      {x:0.076, y:0.039, xref:"x", yref:"y", text:"Epoch 100", showarrow:true, arrowhead:0, arrowcolor:"#ffffff", font:contourAnnotationFont, textposition:"right", ax:60, ay:0},
+      {x:0.072, y:0.065, xref:"x", yref:"y", text:"Epoch 200", showarrow:true, arrowhead:0, arrowcolor:"#ffffff", font:contourAnnotationFont, textposition:"right", ax:60, ay:0}
+    ]
   })
 }
 
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 1, inputShape: [1]}))
-model.compile({optimizer: tf.train.sgd(0.0005), loss:"meanAbsoluteError"});
+// move the white dots from "Start" to "Epoch 20", "Epoch 100" etc. locations
+function updateContourWithTrace(dataTrace, k, b, N, traceIndex) {
+  dataTrace.x = [k];
+  dataTrace.y = [b];
+  var update = {x: [dataTrace.x], y:[dataTrace.y], name:'model after ' + N + ' epochs'}
+  Plotly.restyle('lossSurface2dWithTraj', update, traceIndex)
+}
+
+
 let k = 0;
 let b = 0;
-setModelWeights(0, 0);
 genLossLandscape();
-
+setModelWeights(0, 0);
 
 (async () => {
   await model.fit(trainTensors.sizeMB, trainTensors.timeSec, {
@@ -132,19 +169,30 @@ genLossLandscape();
     callbacks: {
       onEpochEnd: async (epoch, logs) => {
         plotLoss(epoch+1, logs.loss);
+        plotParams(epoch+1, k, b);
         recordTrajectory(k, b, logs.loss)
         k = model.getWeights()[0].dataSync()[0]
         b = model.getWeights()[1].dataSync()[0]
+        if (epoch===0) {
+          updateContourWithTrace(trace0, k, b, 0, 2)
+        }
         if (epoch === 9) {
           updateScatterWithLines(dataTrace10Epochs, k, b, 10, 2);
           console.log("wrote model 10")
         }
         if (epoch===19) {
           updateScatterWithLines(dataTrace20Epochs, k, b, 20, 3);
+          updateContourWithTrace(trace20, k, b, 20, 3)
           console.log("wrote model 20")
         }
+        if (epoch===99) {
+          updateScatterWithLines(dataTrace100Epochs, k, b, 100, 4)
+          updateContourWithTrace(trace100, k, b, 100, 4)
+          console.log("wrote model 100")
+        }
         if (epoch === 199) {
-          updateScatterWithLines(dataTrace200Epochs, k, b, 200, 4);
+          updateScatterWithLines(dataTrace200Epochs, k, b, 200, 5);
+          updateContourWithTrace(trace200, k, b, 200, 5)
           console.log("wrote model 200")
         }
         await tf.nextFrame();
